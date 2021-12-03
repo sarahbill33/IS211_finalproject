41dcb5a347ed464e7503e419459b4350cd7a36a0
Sarah Bill <sarahbill33@gmail.com>
2021-12-03 13:14:42 -0500

    basic framework

---
+++ bookapp.py
@@ -0,0 +1,12 @@
+# Login
+# List of books owned (title, author, page count, average rating)
+# Add a book by searching Google Book API via ISBN
+# https://www.googleapis.com/books/v1/volumes?q=isbn:9781449372620
+# return appropriate error messages
+# delete books from list
+# The purpose of this web application is to provide the end user with a fuctional interface that will allow
+# EC: support multiple users (table with username, pass)
+# EC: save links to thumbnails and display them
+# EC:let user choose from multiple responses when adding a book
+# EC: allow searching by title
+
