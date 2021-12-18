b059ed13967763de2bcd56055ad06faae38f4d8f
Sarah Bill <sarahbill33@gmail.com>
2021-12-17 21:52:21 -0500

    created class for book db

--- bookapp.py
+++ bookapp.py
@@ -4,9 +4,28 @@
 # https://www.googleapis.com/books/v1/volumes?q=isbn:9781449372620
 # return appropriate error messages
 # delete books from list
-# The purpose of this web application is to provide the end user with a fuctional interface that will allow
 # EC: support multiple users (table with username, pass)
 # EC: save links to thumbnails and display them
 # EC:let user choose from multiple responses when adding a book
 # EC: allow searching by title
 
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hw13.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    pagecount = db.Column(db.Integer, nullable=False)
+    avgrating = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, title, author, pagecount, avgrating):
+        self.title = title
+        self.author = author
+        self.pagecount = pagecount
