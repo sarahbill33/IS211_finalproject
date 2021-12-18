b441de4f15af3fdbfe78f3209951125765cf7b73
Sarah Bill <sarahbill33@gmail.com>
2021-12-18 16:29:46 -0500

    getting unformatted api response

--- bookapp.py
+++ bookapp.py
@@ -8,12 +8,13 @@
 # EC: save links to thumbnails and display them
 # EC:let user choose from multiple responses when adding a book
 # EC: allow searching by title
-
+import urllib.request
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hw13.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -29,6 +30,7 @@ class Books(db.Model):
         self.title = title
         self.author = author
         self.pagecount = pagecount
+        self.avgrating = avgrating
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -38,8 +40,49 @@ def login():
         userid = request.form['userid']
         password = request.form['password']
         if userid == 'admin' and password == 'password':
-            return redirect('/dashboard')
+            return redirect('/searchbook')
         else:
             return render_template("index.html", title=title)
     else:
         return render_template("index.html", title=title)
+
+
+@app.route('/searchbook', methods=['POST', 'GET'])
+def searchbook():
+    title = "Searchbook"
+    if request.method == 'POST':
+        isbn = request.form['ISBN']
+        try:
+            r = urllib.request.urlopen(f'https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}')
+            response = r.read()
+            return response
+        except:
+            response = "That ISBN does not exist in our records. Try again."
+            return response
+    else:
+        return render_template("searchbook.html", title=title)
+
+
+@app.route('/addbook', methods=['POST', 'GET'])
+def addbook():
+    title = "Addbook"
+    if request.method == 'POST':
+        title = "title"
+        author = "author"
+        pagecount = 1
+        avgrating = 5
+        newbook = Books(title=title, author=author, pagecount=pagecount, avgrating=avgrating)
+        try:
+            db.session.add(newbook)
+            db.session.commit()
+            return redirect('/searchbook')
+        except:
+            return "There was an error adding your book"
+    else:
+        return render_template("searchbook.html", title=title)
+
+
+if __name__ == '__main__':
+    db.init_app(app)
+    db.create_all()
+    app.run(debug=True)
